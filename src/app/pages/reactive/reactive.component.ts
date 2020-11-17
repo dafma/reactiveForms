f87ab817d79216a3549cfe8fc17525ac1a09be8b
dafma <mar_tp@hotmail.es>
2020-11-17 11:03:28 -0600

    formularios reactivos

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive.component.css']
 })
 export class ReactiveComponent implements OnInit {
+  forma: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.crearFormulario();
+  }
 
   ngOnInit(): void {
+  }
+
+  crearFormulario() {
+    this.forma = this.fb.group({
+      nombre  : ['reads'],
+      apellido: ['rom'],
+      correo  : ['reads@gmail.com'],
+    });
+  }
+
+  guardar(){
+    console.log(this.forma);
+    
   }
 
 }
